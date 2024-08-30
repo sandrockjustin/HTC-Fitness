ad97e3b7e7cd26971207137cbb7d7aca31d30f5f
AJ-Gamer <ajohnsonjr0405@gmail.com>
2024-08-30 15:00:25 -0500

    Connect database to server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const path = require('path')
+const mongoose = require('mongoose');
 
 const PORT = 3000;
 const DIST_DIR = path.resolve(__dirname, '../dist/client');
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/HTC-Fitness')
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err.message);
+});
 
 app.use(express.json());
 app.use(express.static(DIST_DIR));
