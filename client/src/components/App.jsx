e2004992c8cd91a85ed7495738001bd1eac3546e
mothroom <dakotaday1997@gmail.com>
2024-08-30 15:56:47 -0500

    ADD / navbar with routers that go to each component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,27 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
+
+import NavBar from "./NavBar.jsx";
+import HomePage from "./HomePage.jsx";
+import Goals from "./Goals.jsx";
+import Routines from "./Routines.jsx";
 
 const App = () => {
-    return (
-        <div>Hello World!</div>
-    )
-}
+  return (
+    <div className="app-view">
+      <Router>
+        <div className="router">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/routines" element={<Routines />} />
+            <Route path="/goals" element={<Goals />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+};
 
 export default App
