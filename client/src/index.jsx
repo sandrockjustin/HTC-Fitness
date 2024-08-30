b58c11dc4485f4d2f9c0f2d5f63a3899af450c63
AJ-Gamer <ajohnsonjr0405@gmail.com>
2024-08-30 11:12:32 -0500

    Set up client side, Bug with rendering page

---
+++ client/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './components/App';
+const $root = document.getElementById('root')
+if ($root) {
+    const root = createRoot($root)
+    root.render(<App />);
+} else {
+    throw new Error('Element root not found.')
+}
