52c83f7bf87c73489372b58f68832a66a8c4f3a2
mothroom <dakotaday1997@gmail.com>
2024-08-30 11:38:48 -0500

    BUGFIX / App renders to '/' endpoint

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path')
+
 const PORT = 3000;
-const DIST_DIR = path.resolve(__dirname, '..', 'dist');
+const DIST_DIR = path.resolve(__dirname, '../dist/client');
 const app = express();
 
 app.use(express.json());
 app.use(express.static(DIST_DIR));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(DIST_DIR, 'index.html'))
+})
+
 app.listen(PORT, () => {
     console.info(`Server listening at http://127.0.0.1:${PORT}`)
 });
-  
