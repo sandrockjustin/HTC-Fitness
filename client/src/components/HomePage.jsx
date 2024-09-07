d9c3445811a88779e2a90de21169f820a2d62332
mothroom <dakotaday1997@gmail.com>
2024-09-07 12:28:44 -0500

    UPDATE / Changed to check if user is set and then renders user name

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -2,25 +2,29 @@ import React from 'react';
 import { Grid2, Typography } from '@mui/material';
 import ExerciseCard from './ExerciseCard.jsx';
 
-const HomePage = ({ exercises }) => (
-  <div>
-    <Typography variant="h4" gutterBottom align="center">
-    {'Welcome, <User>, to the Hyperbolic Time Chamber'}
-    </Typography>
-    {exercises.length > 0 ? (
-        <Grid2 container spacing={2} alignItems="stretch" paddingLeft="6rem">
-          {exercises.map((exercise, index) => (
-            <Grid2 xs={12} sm={6} md={4} key={index}>
-              <ExerciseCard exercise={ exercise }/>
-            </Grid2>
-          ))}
-        </Grid2>
-    ) : (
-      <Typography variant="body1">
-      No exercises found.
-    </Typography>
-    )}
-  </div>
-);
+const HomePage = ({ user, exercises }) => {
+  const userName = user ? user.nameFirst : '';
+
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom align="center">
+      {`Welcome, ${userName}, to the Hyperbolic Time Chamber`}
+      </Typography>
+      {exercises.length > 0 ? (
+          <Grid2 container spacing={2} alignItems="stretch" paddingLeft="6rem">
+            {exercises.map((exercise, index) => (
+              <Grid2 xs={12} sm={6} md={4} key={index}>
+                <ExerciseCard exercise={ exercise }/>
+              </Grid2>
+            ))}
+          </Grid2>
+      ) : (
+        <Typography variant="body1">
+        No exercises found.
+      </Typography>
+      )}
+    </div>
+  );
+};
 
 export default HomePage;
