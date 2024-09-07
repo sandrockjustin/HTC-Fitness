f55b3e80e5c7ce6d0820ac63c3559e603b82348e
mothroom <dakotaday1997@gmail.com>
2024-09-07 12:29:55 -0500

    UPDATE / Goal page components now all work with user stored data

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -1,47 +1,62 @@
 import React from 'react';
 import { LineChart, ChartsReferenceLine } from '@mui/x-charts';
 
-const sampleData = [
-  { date: '2024-08-01', weight: 250 },
-  { date: '2024-08-02', weight: 248 },
-  { date: '2024-08-03', weight: 248.2 },
-  { date: '2024-08-04', weight: 249 },
-  { date: '2024-08-05', weight: 246.7 },
-  { date: '2024-08-06', weight: 246 },
-  { date: '2024-08-07', weight: 248.1 },
-];
+const Chart = ({ weights, goalWeight }) => {
+  const formattedData = weights.map((weightEntry) => ({
+    date: new Date(weightEntry.date).toISOString().split('T')[0],
+    // Format date as YYYY-MM-DD
+    weight: parseFloat(weightEntry.weight),
+  }));
+  const allWeights = weights.map((weightEntry) => weightEntry.weight);
+  // Calculate the minimum and maximum weights, including the goal weight
+  const actualMinWeight = Math.min(...allWeights);
+  const actualMaxWeight = Math.max(...allWeights);
 
-const goalWeight = 248;
+  // Add padding to min and max weights, but ensure that it also considers goalWeight
+  const minWeight = actualMinWeight === Math.min(...allWeights, goalWeight)
+    ? actualMinWeight - 5
+    : Math.min(actualMinWeight, goalWeight - 5);
 
-const Chart = () => (
-  <LineChart
-    dataset={sampleData}
-    xAxis={[{
-      dataKey: 'date',
-      scaleType: 'band',
-      label: 'Date',
-    }]}
-    series={[{
-      dataKey: 'weight',
-      label: 'Weight (lbs.)',
-      color: '#1976d2',
-    }]}
-    height={600}
-    grid={{ vertical: true, horizontal: true }}
-    sx={{
-      backgroundColor: '#303030',
-      borderRadius: '1rem',
-      padding: '1rem',
-      mt: 2,
-    }}
-  >
-    <ChartsReferenceLine
-      y={goalWeight}
-      label={`Goal Weight: ${goalWeight} lbs`}
-      lineStyle={{ stroke: 'orange', strokeWidth: 2, strokeDasharray: '4 4' }}
-      labelAlign='start'
-    />
-  </LineChart>
-);
+  const maxWeight = actualMaxWeight === Math.max(...allWeights, goalWeight)
+    ? actualMaxWeight + 5
+    : Math.max(actualMaxWeight, goalWeight + 5);
+
+  return (
+    <LineChart
+      dataset={formattedData}
+      xAxis={[{
+        dataKey: 'date',
+        scaleType: 'band',
+        label: 'Date',
+      }]}
+      yAxis={[{
+        min: minWeight,
+        max: maxWeight,
+      }]}
+      series={[{
+        dataKey: 'weight',
+        label: 'Weight (lbs.)',
+        color: '#1976d2',
+      }]}
+      height={600}
+      grid={{ vertical: true, horizontal: true }}
+      sx={{
+        backgroundColor: '#303030',
+        borderRadius: '1rem',
+        padding: '1rem',
+        mt: 2,
+      }}
+    >
+      {goalWeight > 0 && (
+        <ChartsReferenceLine
+          y={goalWeight}
+          label={`Goal Weight: ${goalWeight} lbs`}
+          lineStyle={{ stroke: 'orange', strokeWidth: 2, strokeDasharray: '4 4' }}
+          labelAlign='start'
+      />
+      )}
+    </LineChart>
+  );
+};
 
 export default Chart;
--- client/src/components/WeightCard.jsx
+++ client/src/components/WeightCard.jsx
@@ -11,74 +11,117 @@ import {
 } from '@mui/material';
 import dayjs from 'dayjs';
 import { DatePicker } from '@mui/x-date-pickers';
+import axios from 'axios';
 
 const WeightCard = ({
+  userId,
   title,
   weight,
   showInput,
   onButtonClick,
-  onInputChange,
-  onSubmit,
   onCancel,
-  buttonText,
   inputLabel,
   isCurrentWeightCard,
   onAddDateWeight,
+  onGoalWeightUpdate,
 }) => {
   const [date, setDate] = useState(dayjs());
   const [dateWeight, setDateWeight] = useState('');
+  const [goalWeight, setGoalWeight] = useState('');
   const [openDateDialog, setOpenDateDialog] = useState(false);
 
-  const handleDateSubmit = () => {
+  const handleDateSubmit = async () => {
     if (date && dateWeight) {
-      onAddDateWeight(date, dateWeight);
-      setOpenDateDialog(false);
-      setDate(dayjs());
-      setDateWeight('');
+      try {
+        await axios.patch(`/api/users/${userId}/weights`, {
+          weights: [{ weight: dateWeight, date: date.toISOString() }],
+        });
+        onAddDateWeight(date, dateWeight);
+        setOpenDateDialog(false);
+        setDate(dayjs());
+        setDateWeight('');
+      } catch (error) {
+        throw new Error(error);
+      }
+    }
+  };
+
+  const handleGoalSubmit = async () => {
+    if (goalWeight) {
+      try {
+        await axios.patch(`/api/users/${userId}`, {
+          goal_weight: goalWeight,
+        });
+        if (onGoalWeightUpdate) {
+          onGoalWeightUpdate(goalWeight); // Notify parent component
+        }
+        setGoalWeight('');
+        onCancel();
+      } catch (error) {
+        throw new Error(error);
+      }
+    }
+  };
+
+  const handleResetGoalWeight = async () => {
+    try {
+      await axios.delete(`/api/users/${userId}/goal-weight`);
+      if (onGoalWeightUpdate) {
+        onGoalWeightUpdate(0);
+      }
+    } catch (error) {
+      throw new Error(error);
     }
   };
 
   return (
     <Grid2 xs={12} md={6}>
-      <Card sx={{
-        height: '100%',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-      }}>
+      <Card
+        sx={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
         <CardContent>
-          <Typography variant="h6">{title}</Typography>
-          <Typography variant="body1">
+          <Typography variant='h4'>{title}</Typography>
+          <Typography variant='subtitle1'>
             {weight ? `${weight} lbs.` : `No ${title.toLowerCase()} set.`}
           </Typography>
         </CardContent>
-        <CardContent >
+        <CardContent>
           {isCurrentWeightCard ? (
             <>
-              <Button variant="contained" onClick={() => setOpenDateDialog(true)}>
+              <Button
+                variant='contained'
+                onClick={() => setOpenDateDialog(true)}
+              >
                 Add Weight
               </Button>
-              <Dialog open={openDateDialog} onClose={() => setOpenDateDialog(false)}>
+              <Dialog
+                open={openDateDialog}
+                onClose={() => setOpenDateDialog(false)}
+              >
                 <Stack spacing={2} sx={{ p: 3 }}>
                   <DatePicker
-                    label="Select Date"
+                    label='Select Date'
                     value={date}
                     onChange={(newDate) => setDate(newDate)}
-                    // renderInput={(params) => <TextField {...params} />}
                   />
                   <TextField
-                    type="number"
-                    label="Weight"
+                    type='number'
+                    label='Weight'
                     value={dateWeight}
                     onChange={(e) => setDateWeight(e.target.value)}
                     fullWidth
                   />
-                  <Stack direction="row" spacing={1}>
-                    <Button variant="contained" onClick={handleDateSubmit}>
+                  <Stack direction='row' spacing={1}>
+                    <Button variant='contained' onClick={handleDateSubmit}>
                       Submit
                     </Button>
-                    <Button variant="outlined" onClick={() => setOpenDateDialog(false)}>
+                    <Button variant='outlined' onClick={() => setOpenDateDialog(false)}>
                       Cancel
                     </Button>
                   </Stack>
@@ -88,25 +131,38 @@ const WeightCard = ({
           ) : (
             <>
               {!showInput ? (
-                <Button variant="contained" color="primary" onClick={onButtonClick}>
-                  {buttonText}
-                </Button>
+                <Stack direction='row' spacing={1}>
+                  <Button
+                    variant='contained'
+                    color='primary'
+                    onClick={onButtonClick}
+                  >
+                    Set Goal Weight
+                  </Button>
+                  <Button
+                    variant='outlined'
+                    color='error'
+                    onClick={handleResetGoalWeight}
+                  >
+                    Reset Goal Weight
+                  </Button>
+                </Stack>
               ) : (
                 <Stack spacing={1} sx={{ mt: 2 }}>
                   <TextField
-                    type="number"
+                    type='number'
                     label={inputLabel}
-                    variant="outlined"
-                    value={weight}
-                    onChange={onInputChange}
+                    variant='outlined'
+                    value={goalWeight}
+                    onChange={(e) => setGoalWeight(e.target.value)}
                     sx={{ mb: 2, mr: 1 }}
                     fullWidth
                   />
-                  <Stack direction="row" spacing={1}>
-                    <Button variant="contained" color="primary" onClick={onSubmit}>
+                  <Stack direction='row' spacing={1}>
+                    <Button variant='contained' color='primary' onClick={handleGoalSubmit}>
                       Add Weight
                     </Button>
-                    <Button variant="outlined" color="error" onClick={onCancel}>
+                    <Button variant='outlined' color='error' onClick={onCancel}>
                       Cancel
                     </Button>
                   </Stack>
