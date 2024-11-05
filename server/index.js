c8d2a512a7faed9451d8fc6b944505d6baaceb89
sandrockjustin <sandrock.justin@gmail.com>
2024-11-05 13:37:21 -0600

    fix / server - removed conditional from Passport callbackURL

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: `${process.env.GOOGLE_CLIENT_ID}`,
   clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
-  callbackURL: `${process.env.GOOGLE_CALLBACK_URL}` || 'http://localhost:3000/auth/google/callback',
+  callbackURL: 'http://localhost:3000/auth/google/callback',
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     // Check if User profile exists in DB
