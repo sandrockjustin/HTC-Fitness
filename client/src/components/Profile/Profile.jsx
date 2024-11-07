d4dd926c21c75579fcdae75a11c059d6cc436a7a
sandrockjustin <sandrock.justin@gmail.com>
2024-11-07 16:21:50 -0600

    feat / client - Profile.jsx now displays user meetups

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 import BasicProfileInfo from './BasicProfileInfo.jsx';
 import ProfileFriends from './ProfileFriends.jsx';
+import ProfileMeetups from './ProfileMeetups.jsx';
 
 const Profile = (props) => {
 
@@ -29,22 +30,28 @@ const Profile = (props) => {
   }
 
   return (
-    <div id="profilePage" style={{width: 544, padding: "32px"}}>
-      { props.user ?
-        <div id="profileInfo-container">
-          <div id="basicInfo" style={{maxWidth: 480}}>
-            <span style={{paddingBottom: "8px"}}>Your Account</span>
-            <BasicProfileInfo user={props.user}/>
+    <div id="profilePage" style={{display: 'flex', flexDirection: 'row'}}>
+      <div id="profileInfo" style={{width: 544, padding: "32px"}}>
+        { props.user ?
+          <div id="profileInfo-container">
+            <div id="basicInfo" style={{maxWidth: 480}}>
+              <span style={{paddingBottom: "8px"}}>Your Account</span>
+              <BasicProfileInfo user={props.user}/>
+            </div>
+            <div id="friendsDisplay" style={{display: "flex", maxWidth: 520}}>
+              <ProfileFriends user={props.user} removeFriend={removeFriend}/>
+            </div>
           </div>
-          <div id="friendsDisplay" style={{display: "flex", maxWidth: 520}}>
-            <ProfileFriends user={props.user} removeFriend={removeFriend}/>
-          </div>
-        </div>
-        :
-        <div>
-          401; user is not authenticated.
-        </div>        
-      }
+          :
+          <div>
+            401; user is not authenticated.
+          </div>        
+        }
+      </div>
+      <div id="profileMeetups"style={{display: "flex", flexDirection: 'column', paddingTop:"32px", width: "70%"}}>
+        <span style={{}}>Subscribed Meetups</span>
+        <ProfileMeetups user={props.user}/>
+      </div>
     </div>
   )
 
